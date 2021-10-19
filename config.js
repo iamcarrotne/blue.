d4d3b8aacef8bb6b202344827433f53c235b5349
iamcarrotne <92147410+iamcarrotne@users.noreply.github.com>
2021-10-19 14:09:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const CONFIG = {
     introDesc: `HAPPY BIRTHDAY MY BABI ^^`,
     btnIntro: 'tiếp nè ^^',
     title: 'Úi có bạn nào đang đọc bức thư này đã bước qua tuổi 18 rồi nè ^^',
-    desc: ' Chúc babi có một ngày sinh nhật siêu vui vẻ <3. Chúc dtin qua tuổi mới luôn đẹp zai, học giỏi, có nhiều sức khoẻ nè và thực hiện được những thứ dtin mơ ước nhe. Cảm ơn dtin vì luôn ở bên cạnh em lúc buồn, luôn nghe em tâm sự. Mong sau này mọi chuyện quan trọng của em đều có sự xuất hiện của anh hihi ^^',
+    desc: ' Chúc babi có một ngày sinh nhật siêu vui vẻ <3. Chúc dtin qua tuổi mới luôn đẹp zai, học giỏi, có nhiều sức khoẻ nè và thực hiện được những thứ dtin mơ ước nhe, à phải iu em nhìu nữa hì :3 . Cảm ơn dtin vì luôn ở bên cạnh em lúc buồn, luôn nghe em tâm sự. Mong sau này mọi chuyện quan trọng của em đều có sự xuất hiện của anh hihi ^^',
     btnYes: 'iu carrot nhìu nhìu hihi :3',
     btnNo: 'Khum iu carrot hic :<',
     question:'Khung điều ước :>',
